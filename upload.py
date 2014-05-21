c359ea186bf0fdda9f3f8e9139e075d6a69ee472
faulpaul <github@goessinger.eu>
2014-05-21 08:39:30 +0200

    some old stuff

---
+++ upload.py
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html><head><title>File Upload</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head><body><h1>File Upload</h1>
+<form action="/scripts/upload.py" method="POST" enctype="multipart/form-data">
+File name: <input name="file_1" type="file"><br>
+File name: <input name="file_2" type="file"><br>
+File name: <input name="file_3" type="file"><br>
+<input name="submit" type="submit">
+</form>
+</body>
+</html>
